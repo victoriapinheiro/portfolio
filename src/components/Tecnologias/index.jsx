e26717f8f988301d350a115e5be4355a14f272e0
Victoria Vivian <victoriaviviancp@gmail.com>
2025-07-19 13:54:06 -0300

    refactor(tecnologias): melhora a semantica do scroll horizontal

--- src/components/Tecnologias/index.jsx
+++ src/components/Tecnologias/index.jsx
@@ -1,61 +1,186 @@
-import Head from "next/head";
 import styles from "./tecnologias.module.css";
 import Image from "next/image";
-import html_icon from "@/app/assets/tec_icons/icone-html-noir.png";
+import html_icon from "@/app/assets/tec_icons/icone-html5.png";
+import css_icon from "@/app/assets/tec_icons/icone-css3.png";
+import js_icon from "@/app/assets/tec_icons/icone-javascript.png";
+import react_icon from "@/app/assets/tec_icons/icone-react.png";
+import nextjs_icon from "@/app/assets/tec_icons/icone-nextjs.png";
+import git_icon from "@/app/assets/tec_icons/icone-git.png";
+import github_icon from "@/app/assets/tec_icons/icone-github.png";
 
 export default function Home() {
   return (
     <>
       <div className={styles.tecnologias}>
         <div className={styles.scroller__inner}>
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
-          <Image src={html_icon} />
+          <div className={styles.scroller__item}>
+            <Image src={html_icon} />
+            <figcaption>HTML</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={css_icon} />
+            <figcaption>CSS</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={js_icon} />
+            <figcaption>JavaScript</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={react_icon} />
+            <figcaption>REACT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={nextjs_icon} />
+            <figcaption>NEXT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={git_icon} />
+            <figcaption>GIT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={github_icon} />
+            <figcaption>GITHUB</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={html_icon} />
+            <figcaption>HTML</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={css_icon} />
+            <figcaption>CSS</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={js_icon} />
+            <figcaption>JavaScript</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={react_icon} />
+            <figcaption>REACT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={nextjs_icon} />
+            <figcaption>NEXT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={git_icon} />
+            <figcaption>GIT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={github_icon} />
+            <figcaption>GITHUB</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={html_icon} />
+            <figcaption>HTML</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={css_icon} />
+            <figcaption>CSS</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={js_icon} />
+            <figcaption>JavaScript</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={react_icon} />
+            <figcaption>REACT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={nextjs_icon} />
+            <figcaption>NEXT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={git_icon} />
+            <figcaption>GIT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={github_icon} />
+            <figcaption>GITHUB</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={html_icon} />
+            <figcaption>HTML</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={css_icon} />
+            <figcaption>CSS</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={js_icon} />
+            <figcaption>JavaScript</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={react_icon} />
+            <figcaption>REACT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={nextjs_icon} />
+            <figcaption>NEXT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={git_icon} />
+            <figcaption>GIT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={github_icon} />
+            <figcaption>GITHUB</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={html_icon} />
+            <figcaption>HTML</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={css_icon} />
+            <figcaption>CSS</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={js_icon} />
+            <figcaption>JavaScript</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={react_icon} />
+            <figcaption>REACT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={nextjs_icon} />
+            <figcaption>NEXT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={git_icon} />
+            <figcaption>GIT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={github_icon} />
+            <figcaption>GITHUB</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={html_icon} />
+            <figcaption>HTML</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={css_icon} />
+            <figcaption>CSS</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={js_icon} />
+            <figcaption>JavaScript</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={react_icon} />
+            <figcaption>REACT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={nextjs_icon} />
+            <figcaption>NEXT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={git_icon} />
+            <figcaption>GIT</figcaption>
+          </div>
+          <div className={styles.scroller__item}>
+            <Image src={github_icon} />
+            <figcaption>GITHUB</figcaption>
+          </div>
         </div>
       </div>
     </>
